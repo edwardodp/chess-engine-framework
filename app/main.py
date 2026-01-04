009066594b307f59f0c39d00a1638062ad18200d
Jakub Kisielewski <186347180+Jakub-Kisielewski@users.noreply.github.com>
2026-01-04 22:07:13 +0000

    wrapper eval func array conversion

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 """ The main entry point and the 'backend' of sorts """
 import numpy as np
-from numba import cfunc, types, carray, njit
+from numba import cfunc, types, carray
 
+# return 32 bit signed int, 1 parameter - pointer of type 8 bit signed int (pointing to C board data array)
 @cfunc(types.int32(types.CPointer(types.int8)))
+# wrapper evalutation function
 def _evalutation_function(dataPtr):
+
+    # convert c array to numpy 2d (8x8) array
+    boardData = np.reshape(carray(dataPtr, (4,), np.int8), (2,2))
     
+    return 0
+
+
 
