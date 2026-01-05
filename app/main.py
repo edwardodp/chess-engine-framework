f249e7d26db43401f4135e074395986bd42c9818
Jakub Kisielewski <186347180+Jakub-Kisielewski@users.noreply.github.com>
2026-01-05 22:43:46 +0000

    updated wrapper eval func to changed input and entry point code

--- app/main.py
+++ app/main.py
@@ -6,20 +6,17 @@ import ctypes
 import sys 
 import os
 
-# 12 64 bit integers, each integer is a type of piece e.g white pawn, black pawn. and each bit represents a cell on board where 1 means it is on that cell. a1, b1 , ... h1, a2, ... , h2
-# white are first 6, pawn knight bishop rook queen king, black are last 6 with same order
-
-# return 32 bit signed int, 1 parameter - pointer of type 8 bit signed int (pointing to C board data array)
-@cfunc(types.int32(types.CPointer(types.int8)))
+# return 32 bit signed int, 1 parameter - pointer of type 64 bit unsigned int (pointing to C board data array)
+@cfunc(types.int32(types.CPointer(types.uint64)))
 # wrapper evalutation function
 def _evalutation_function(dataPtr):
 
-    # convert c array to numpy 2d (8x8) array
-    boardData = np.reshape(carray(dataPtr, (64,), np.int8), (8,8))
+    # convert c array to numpy array
+    boardData = carray(dataPtr, (12,), np.uint64)
     
-    evaluation = np.int32(evaluationFunction(boardData))    
+    evaluation = np.int32(evaluationFunction(boardData))
     
-    return 0
+    return evaluation
 
 # check OS to fetch corresponding library
 if sys.platform == "win32":
@@ -27,6 +24,7 @@ if sys.platform == "win32":
     init = ctypes.CDLL(libPath)
 else:
     libPath = os.path.abspath("example.so")
+
     init = ctypes.CDLL(libPath)
 
 # define parameter type and initialise engine
