52a1319009eeb6bc83e84121082979d5224680af
Jakub Kisielewski <kisielewskij66@gmail.com>
2026-01-14 13:06:08 +0000

    added getting peices

--- app/evaluation.py
+++ app/evaluation.py
@@ -1,10 +1,9 @@
-""" Competitors edit this file (evaluation function) """
 import numpy as np
 from numba import njit
 import boardTools as bt
 
 @njit
-def evaluationFunction(boardData):
+def evaluation_function(board_data):
     evaluation = 0
     # Your code goes here!
 
